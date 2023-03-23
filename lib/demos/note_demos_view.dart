a8af9f6125c2f406f48d1a33ad14019c18285a14
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-03-23 21:01:13 +0300

    Note Taking App View

---
+++ lib/demos/note_demos_view.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/101/image_learn.dart';
+
+class NoteDemos extends StatelessWidget {
+  const NoteDemos({super.key});
+  final _title = 'Create your first note';
+  final _description = 'Add a note';
+  final _createNote = 'Create a note';
+  final _importNotes = 'Import Notes';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[50],
+      appBar: AppBar(),
+      body: Padding(
+        padding: PaddingItems().horizontalPadding,
+        child: Column(
+          children: [
+            PngImage(path: ImageItems().appleWithBook),
+            _TitleWidget(title: _title),
+            Padding(
+              padding: PaddingItems().verticalPadding,
+              child: _SubtitleWidget(text: _description * 10),
+            ),
+            const Spacer(),
+            _createButton(createNote: _createNote),
+            TextButton(onPressed: () {}, child: Text(_importNotes)),
+            SizedBox(height: ButtonHeights().buttonNormalHeight),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _createButton extends StatelessWidget {
+  const _createButton({
+    super.key,
+    required String createNote,
+  }) : _createNote = createNote;
+
+  final String _createNote;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: SizedBox(
+        height: ButtonHeights().buttonNormalHeight,
+        child: Center(
+          child: Text(
+            _createNote,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Center text widget
+class _SubtitleWidget extends StatelessWidget {
+  const _SubtitleWidget(
+      {super.key, this.textAlign = TextAlign.center, required this.text});
+  final TextAlign? textAlign;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.subtitle1?.copyWith(
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+          ),
+    );
+  }
+}
+
+class _TitleWidget extends StatelessWidget {
+  const _TitleWidget({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+            color: Colors.black,
+            fontWeight: FontWeight.w800,
+          ),
+    );
+  }
+}
+
+class PaddingItems {
+  final EdgeInsets horizontalPadding = EdgeInsets.symmetric(horizontal: 20);
+  final EdgeInsets verticalPadding = EdgeInsets.symmetric(vertical: 10);
+}
+
+class ButtonHeights {
+  final double buttonNormalHeight = 50;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_full_learn/101/image_learn.dart';
 import 'package:flutter_full_learn/101/padding_learn.dart';
 import 'package:flutter_full_learn/101/text_learn_view.dart';
 import 'package:flutter_full_learn/101/container_sized_box_learn.dart';
+import 'package:flutter_full_learn/demos/note_demos_view.dart';
 import '101/icon_learn.dart';
 import '101/scaffold_learn.dart';
 import '101/button_learn.dart';
@@ -27,11 +28,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
           appBarTheme: const AppBarTheme(
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.transparent,
         elevation: 0,
-        systemOverlayStyle: SystemUiOverlayStyle.light,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
       )),
-      home: ImageLearn(),
+      home: NoteDemos(),
     );
   }
 }
