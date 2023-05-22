3d1cfb69712955261dc030012fb04c3a6db8db39
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-05-23 01:18:25 +0300

    Stateful Learn

---
+++ lib/101/stateful_learn.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/product/counter_hello_button.dart';
+import 'package:flutter_full_learn/product/language/language_items.dart';
+
+class StatefulLearn extends StatefulWidget {
+  const StatefulLearn({super.key});
+
+  @override
+  State<StatefulLearn> createState() => _StatefulLearnState();
+}
+
+class _StatefulLearnState extends State<StatefulLearn> {
+  int _counter = 0;
+
+  void _updateCounter(bool isIncrement) {
+    if (isIncrement) {
+      _counter++;
+    } else {
+      _counter--;
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(LanguageItems.welcomeTitle),
+      ),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _incrementButton(),
+          _deincrementButton(),
+        ],
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(_counter.toString(), style: Theme.of(context).textTheme.headlineLarge),
+          ),
+          const Placeholder(),
+          const CounterHelloButton(),
+        ],
+      ),
+    );
+  }
+
+  FloatingActionButton _incrementButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        _updateCounter(true);
+      },
+      child: const Icon(Icons.add),
+    );
+  }
+
+  Padding _deincrementButton() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: FloatingActionButton(
+        onPressed: () {
+          _updateCounter(false);
+        },
+        child: const Icon(Icons.remove),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_full_learn/101/indicator_learn.dart';
 import 'package:flutter_full_learn/101/list_tile_learn.dart';
 import 'package:flutter_full_learn/101/padding_learn.dart';
 import 'package:flutter_full_learn/101/stack_learn.dart';
+import 'package:flutter_full_learn/101/stateful_learn.dart';
 import 'package:flutter_full_learn/101/text_learn_view.dart';
 import 'package:flutter_full_learn/101/container_sized_box_learn.dart';
 import 'package:flutter_full_learn/demos/note_demos_view.dart';
@@ -32,17 +33,15 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-          progressIndicatorTheme:
-              const ProgressIndicatorThemeData(color: Colors.amber),
-          listTileTheme:
-              const ListTileThemeData(contentPadding: EdgeInsets.zero),
+          progressIndicatorTheme: const ProgressIndicatorThemeData(color: Colors.amber),
+          listTileTheme: const ListTileThemeData(contentPadding: EdgeInsets.zero),
           appBarTheme: const AppBarTheme(
             centerTitle: true,
             backgroundColor: Colors.transparent,
             elevation: 0,
             systemOverlayStyle: SystemUiOverlayStyle.dark,
           )),
-      home: StackDemoView(),
+      home: StatefulLearn(),
     );
   }
 }
